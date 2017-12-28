14c40deedc6902ecb4c4a5661a0d4e80916d4b5d
JulianChu <yulang.chu@gmail.com>
2017-12-28 22:20:45 +0800

    using RabbitMQ to communication between API Gateway and SayHelloService

--- SayHelloService/Program.cs
+++ SayHelloService/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace SayHelloService
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel()) {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Received: {message}");
+                    Console.WriteLine($"Hello, {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Pess any key to exit");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ WebApplication1/Controllers/SayHelloController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace APIGateway.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/SayHello")]
+    public class SayHelloController : Controller
+    {
+        // GET: api/SayHello/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/SayHello
+        [HttpPost]
+        public string Post([FromQuery]string name)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    string message = name;
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello", basicProperties: null,
+                        body: body);
+                }
+            }
+
+            return $"Hello,{name}";
+        }
+        
+
+    }
+}
