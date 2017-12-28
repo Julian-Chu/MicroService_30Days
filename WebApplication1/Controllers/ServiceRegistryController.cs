e9fae871695a83726509256aa07bee1fde9ee691
JulianChu <yulang.chu@gmail.com>
2017-12-28 21:27:24 +0800

    Add project files.

---
+++ WebApplication1/Controllers/ServiceRegistryController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using APIGateway.Models;
+
+namespace APIGateway.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ServiceRegistry")]
+    public class ServiceRegistryController : Controller
+    {
+        static List<MicroService> serviceList = new List<MicroService>() {
+            new MicroService() { Name="API Gateway", Location="http://localhost:2324" }
+        };
+        // GET: api/ServiceRegistry
+        [HttpGet]
+        public IEnumerable<MicroService> Get()
+        {
+            return serviceList;
+        }
+
+        // POST: api/ServiceRegistry
+        [HttpPost]
+        public void Post([FromBody]MicroService service)
+        {
+            serviceList.Add(service);
+        }
+
+        // DELETE: api/ServiceRegistry/{serviceName}
+        [HttpDelete("{serviceName}")]
+        public void Delete(string serviceName)
+        {
+            serviceList.Remove(serviceList.First(s => s.Name == serviceName));
+        }
+    }
+}
---
+++ WebApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ProductService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+
+            BuildWebHost(args).Run();
+            Console.WriteLine("2");
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:1111")
+                .Build();
+    }
+}
