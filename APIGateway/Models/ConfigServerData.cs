3cfd3525f170210f17d7172b416b560f93697629
JulianChu <yulang.chu@gmail.com>
2018-01-03 16:16:56 +0100

    modify code for core2.0 and change folder name

---
+++ APIGateway/Models/ConfigServerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGateway.Models
+{
+    public class ConfigServerData
+    {
+      public string Bar { get; set; }
+      public string Foo { get; set; }
+      public Info Info { get; set; }
+    }
+
+  public class Info
+  {
+    public string Description { get; set; }
+    public string Url { get; set; }
+  }
+}
---
+++ APIGateway/Startup.cs
@@ -0,0 +1,73 @@
+﻿using APIGateway.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Steeltoe.Extensions.Configuration;
+using Steeltoe.Extensions.Configuration.ConfigServer;
+
+namespace APIGateway
+{
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
+    {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+        .AddEnvironmentVariables()
+        .AddConfigServer();
+
+      Configuration = builder.Build();
+    }
+
+    //public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; set; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddOptions();
+      services.AddMvc();
+      services.Configure<ConfigServerData>(Configuration);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseBrowserLink();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      appLifetime.ApplicationStarted.Register(OnStarted);
+      appLifetime.ApplicationStopped.Register(OnStopped);
+
+    }
+
+    private void OnStopped()
+    {
+      RabbitMQListener.Stop();
+    }
+
+    private void OnStarted()
+    {
+      RabbitMQListener.Start();
+    }
+  }
+}
